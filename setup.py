dd97e1cc81822d753293f4d6cb2124c1ea08233a
francium <varunhooda1@gmail.com>
2018-02-06 23:40:46 -0500

    misc: Fix setup.py syntax error

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='weewx-orm',
     packages=['weewx_orm'],
-    version='1.0.0'
+    version='1.0.0',
     include_package_data=True,
     install_requires=[
         'marshmallow-sqlalchemy',
