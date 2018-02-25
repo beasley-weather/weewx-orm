60ed0e3bc38b982e846b39f47f496ead0d3850d3
francium <varunhooda1@gmail.com>
2018-02-25 17:36:53 -0500

    misc: Fix setup.py typo

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 setup(
     name='weewx-orm',
     author='francium',
-    description='A library for interfacing with the Weewx database layer.'
-    url='https://github.com/beasley-weather/weewx-orm'
+    description='A library for interfacing with the Weewx database layer.',
+    url='https://github.com/beasley-weather/weewx-orm',
     license='MIT',
     version='1.0.0',
     packages=['weewx_orm'],
