d3174d5494ff46040a88b33c73b4e2c35a7d8482
francium <varunhooda1@gmail.com>
2018-02-25 17:33:23 -0500

    misc: Update setup.py for destribution

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup
 
 setup(
     name='weewx-orm',
-    packages=['weewx_orm'],
+    author='francium',
+    description='A library for interfacing with the Weewx database layer.'
+    url='https://github.com/beasley-weather/weewx-orm'
+    license='MIT',
     version='1.0.0',
+    packages=['weewx_orm'],
     include_package_data=True,
     install_requires=[
         'marshmallow-sqlalchemy',
